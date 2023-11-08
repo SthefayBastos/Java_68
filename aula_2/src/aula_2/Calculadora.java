956a64e797a868140485188c171443a9999507c1
Sthefany Sousa <sthefanyssbastos@hotmail.com>
2023-11-08 14:15:47 -0300

    Operadores

--- aula_2/src/aula_2/Calculadora.java
+++ aula_2/src/aula_2/Calculadora.java
@@ -14,6 +14,14 @@ public class Calculadora {
 		System.out.println("Digite o primeiro número");
 		numero2 = leia.nextDouble();
 		
+		System.out.println("Soma: " + (numero1+numero2));
+		System.out.println("Subtração: " + (numero1-numero2));
+		System.out.println("Multiplicação: " + (numero1*numero2));
+		System.out.println("Divisão: " + (numero1/numero2));
+		System.out.println("Módulo do numero1 (Resto da divisão: " + (numero1%2));
+		System.out.println("Módulo do número2 (Resto da divisão: " + (numero2%2));
+		
+		System.out.println("Potência do numero1 pelo numero 2: " + Math.pow(numero1, numero2));
 		System.out.println("Raiz quadrada do numero 1: " + (Math.sqrt(numero1)));
 		
 		
--- aula_2/src/aula_2/Operadores.java
+++ aula_2/src/aula_2/Operadores.java
@@ -10,6 +10,24 @@ public class Operadores {
 		System.out.println("A soma entre as variáveis numero1 e numero2 é igual a : " + (numero1 + numero2));
 		System.out.println("A soma das entre as variáveis valor1 e valor2 é: " +(valor1+valor2));
 		
+		String nome = "Sthefany";
+		int idade = 24;
+		float media = 3.89535f;
+		
+		int tamanho;
+		
+		System.out.println("A soma das variáveis numero1 e numero2 é igual a : " + (numero1+numero2));
+		System.out.println("A soma das variáveis valor1 e valor 2 é igual a: " + (valor1+valor2));
+		
+		System.out.println("Meu nome é " + nome + "e eu tenho " + idade + "anos." );
+		
+		System.out.println("A média final de %s é %.2f, nome, media");
+		
+		System.out.println("\n Digite o tamanho ");
+		tamanho = leia.nextInt();
+		
+		System.out.println("O valor digitado foi " + tamanho);
+		
 	}
 
 }
