e296c00cb7794411b3598c3ab58d62ee807437dd
Sthefany Sousa <sthefanyssbastos@hotmail.com>
2023-11-08 14:40:52 -0300

    Exercicio 01 - Salário Novo

---
+++ Exe01-Java/src/exe01salarioabono/Exe01SalarioAbono.java
@@ -0,0 +1,19 @@
+package exe01salarioabono;
+
+import java.util.Scanner;
+
+public class Exe01SalarioAbono {
+	static Scanner leia = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		float salario, abono;
+		
+		System.out.println("Digite seu salário: ");
+		salario = leia.nextFloat();
+		System.out.println("Digite o valor de abono: ");
+		abono = leia.nextFloat();
+		System.out.println("O valor do novo salário é: " + (salario+abono));
+				
+	}
+
+}
